c679e26dafb7d0510601d3d549934f33f7246945
Roseice <roseice910528@gmail.com>
2023-09-08 17:56:20 +0800

    在搞

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,30 @@
 <template>
-    <div class="d-flex align-center flex-column">
-        <v-card width="400">
-            <v-container>
-                <v-form v-model="valid" lazy-validation>
-                    <v-alert v-show="loginFail" type="error" class="mb-4" data-test="loginAlert">
-                    You are entering an incorrect email and/or password.
-                    </v-alert>
-                    <v-text-field v-model="formData.email" :rules="emailRules" label="Email" required data-test="email"></v-text-field>
-                    <v-text-field v-model="formData.password" :append-icon="pwShow ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, rules.min]"
-                    :type="pwShow ? 'text' : 'password'" name="input-10-1" label="Password" hint="At least 6 characters" counter @click:append="pwShow = !pwShow"
-                    data-test="password"></v-text-field>
-                    <v-spacer></v-spacer>
-                    <v-btn x-large block :disabled="!valid" color="success" @click="login" data-test="loginBtn">
-                    Login
-                    </v-btn>
-                </v-form>
-            </v-container>
-        </v-card>
-    </div>
+  <v-row justify="center" align="center">
+    <v-col cols="8" sm="6" md="1">
+      <v-card>
+        <img src="@/static/1.png"/>
+      </v-card>
+    </v-col>
+  </v-row>
+  <div class="d-flex align-center flex-column">
+      <v-card width="400">
+          <v-container>
+              <v-form v-model="valid" lazy-validation>
+                  <v-alert v-show="loginFail" type="error" class="mb-4" data-test="loginAlert">
+                  You are entering an incorrect email and/or password.
+                  </v-alert>
+                  <v-text-field v-model="formData.email" :rules="emailRules" label="Email" required data-test="email"></v-text-field>
+                  <v-text-field v-model="formData.password" :append-icon="pwShow ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, rules.min]"
+                  :type="pwShow ? 'text' : 'password'" name="input-10-1" label="Password" hint="At least 6 characters" counter @click:append="pwShow = !pwShow"
+                  data-test="password"></v-text-field>
+                  <v-spacer></v-spacer>
+                  <v-btn x-large block :disabled="!valid" color="pink-accent-1" @click="login" data-test="loginBtn">
+                  Login
+                  </v-btn>
+              </v-form>
+          </v-container>
+      </v-card>
+  </div>
 </template>
 <script setup>
 import { ref, reactive } from "vue";
